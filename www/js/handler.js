215c39ae0f683da34e513793d78031c316676cbe
steve kaplan <stevekaplan123@github.com>
2014-07-23 13:56:52 -0400

    steve_changes

--- www/js/handler.js
+++ www/js/handler.js
@@ -1,15 +1,30 @@
-function hideDivs() {
-    var string = "_categories";
-    var array = ["original", "artist", "piece", "archive"];
-    array.forEach(function(category){
-        document.getElementById(category+string).style.visibility = "hidden";
-    });
+function hideDivs(){
+	var string = "_categories";
+	var array = ["paintings", "original", "artist", "piece"];
+	array.forEach(function(category){
+		document.getElementById(category+string).style.visibility = "hidden";
+	});
 }
+
+
 
 function showDiv(div_to_show)
 {
-    document.getElementById(div_to_show+"_categories").style.visibility = "visible";
+	document.getElementById(div_to_show+"_categories").style.visibility = "visible";
 }
+
+function piece(title, audio_on_load, image_on_load) {
+	this.title = title;
+	this.artist = artist;
+    this.audio_on_load = audio_on_load;
+    this.image_on_load = image_on_load;
+    categories = ["about the artist", "about the piece"];
+    artist_info = {audio_on_load:"", biography:"", career:""};
+    piece_info = {audio_on_load:"", title:"", medium:"", style:"", year:"", dimensions:"", summary:""}; 
+}
+
+var current_piece = new piece("", "", "");
+
 
 
 var handler = {
@@ -18,120 +33,121 @@ var handler = {
 
 load: function(result)
 {
+	
             result = result.toLowerCase();
+            var pieces = [];
 
-            var painting_names = ["blue white", "forget it forget me", "starry night"];
-
-            var original = ["about the artist", "about the piece", "brandeis archives"];
-
-            var piece = ["medium", "style"];
-            
-            var artist = ["biography", "career"];
-            
-            var archive = ["hear the artist"];
-
-            painting_names.forEach(function(name) {
-                    if (result.search(name) > -1)
-                    {
-                        alert("painting change to "+name);
-                        new_title = name.split(' ')[0];
-                        document.getElementById("current_painting").src = new_title+".jpg";
-                        document.getElementById("current_title").innerHTML = new_title;
-                        //change div back to original_categories
-                        hideDivs();
-                        showDiv("original");
-                        return;
-                    }
-                });
-            if (document.getElementById("original_categories").style.visibility != "hidden")
-            {
-                original.forEach(function(utterance) {
-                    if (result.search(utterance) > -1)
-                    {
-                    	showDiv("artist");
-                        hideDivs();
-                        if (result.match(utterance) == "about the artist"){
-                            document.getElementById("about-the-artist").load();
-                                alert("ready state : "+document.getElementById('about-the-artist').readyState);
-                            alert("current src"+document.getElementById('about-the-artist').currentSrc);
-                            document.getElementById("about-the-artist").play();
-                            showDiv("artist");
-                        }
-                        else if (result.match(utterance) == "about the piece") {
-                            document.getElementById("about-the-piece").load();
-                            document.getElementById("about-the-piece").play();
-                            showDiv("piece");
-                        }
-                        else {
-                            document.getElementById("brandeis-archives").load();
-                            document.getElementById("brandeis-archives").play();
-                            showDiv("archive");
-                        }
-                    }
-                });
-            }
-            else if (document.getElementById("artist_categories").style.visibility != "hidden")
-            {
-                artist.forEach(function(utterance) {
-                    if (result.search(utterance) > -1)
-                    {
-                        alert("this is utterance "+utterance);
-                        if (result.match(utterance) == "biography"){
-                            document.getElementById("subcategory").src = "https://s3.amazonaws.com/RamaAudio/aboutartist_born.wav";
-                            document.getElementById("subcategory").load();
-                            alert("ready state : "+document.getElementById('subcategory').readyState);
-                            alert("current src"+document.getElementById('subcategory').currentSrc);
-                            document.getElementById("subcategory").play();
-                        }
-                        else if (result.match(utterance) == "career") {
-                            document.getElementById("subcategory").src = "https://s3.amazonaws.com/RamaAudio/aboutartist_fieldmovement.wav";
-                            document.getElementById("subcategory").load();
-                              alert("ready state : "+document.getElementById('subcategory').readyState);
-                            alert("current src"+document.getElementById('subcategory').currentSrc);
-                            document.getElementById("subcategory").play();
-                        }
-                    }
-                });
-            }
-            else if (document.getElementById("piece_categories").style.visibility != "hidden")
-            {
-                piece.forEach(function(utterance) {
-
-                    if (result.search(utterance) > -1)
-                    {
-                        if (result.match(utterance) == "style"){
-                            alert("style should play now");
-                            document.getElementById("subcategory").src = "https://s3.amazonaws.com/RamaAudio/aboutpiece_style.wav";
-                            document.getElementById("subcategory").load();
-                            alert("ready state : "+document.getElementById('subcategory').readyState);
-                            alert("current src"+document.getElementById('subcategory').currentSrc);
-                            document.getElementById("subcategory").play();
-                        }
-                        else if (result.match(utterance) == "medium") {
-                            document.getElementById("subcategory").src = "https://s3.amazonaws.com/RamaAudio/aboutpiece_materialsdimensions.wav";
-                            document.getElementById("subcategory").load();
-
-                            document.getElementById("subcategory").play();
-                        }
-                    }
-                });
-            }
-            else if (document.getElementById("archive_categories").style.visibility != "hidden")
-            {
-                archive.forEach(function(utterance) {
-                    
-                    if (result.search(utterance) > -1)
-                    {
-                        if (result.match(utterance) == "hear the artist"){
-                            document.getElementById("subcategory").src = "quotes.wav"
-                            document.getElementById("subcategory").load();
-
-                            document.getElementById("subcategory").play();
-                        }
-                
-                    }
-                });
-            }
-}
+            //load database pieces into variable pieces
+    		$.ajax({
+        		type: "GET",
+        		url: "/model/rose",
+    		}).done(function(db_pieces) {
+    				//each item is a piece
+    				db_pieces.forEach(function(item) {
+    					pieces[pieces.length] = item;
+    				});
+					
+				if (document.getElementById("paintings_categories").style.visibility != "hidden")
+				{
+					pieces.forEach(function(piece) {
+						var name = piece.title.toLowerCase();
+						if (result.search(name) > -1)
+						{
+							alert(name);
+							new_name = name.replace(" ", "_");
+							document.getElementById("current").style.visibility = "visible";
+							document.getElementById("current_painting").src = new_name+".jpg";
+							document.getElementById("current_title").innerHTML = name;
+							//change div back to original_categories
+							hideDivs();
+							showDiv("original");
+							current_piece = piece;
+						}
+					});
+				}
+				else if (document.getElementById("original_categories").style.visibility != "hidden")
+				{
+					current_piece.categories.forEach(function(utterance) {
+						utterance = utterance.toLowerCase();
+						if (result.search(utterance) > -1)
+						{
+							hideDivs();
+							if (result.match(utterance) == "about the artist"){
+								document.getElementById("about-the-artist").src = current_piece.artist.audio_on_load;
+								document.getElementById("about-the-artist").play();
+								showDiv("artist");
+							}
+							else if (result.match(utterance) == "about the piece") {
+								document.getElementById("about-the-piece").src = current_piece.piece_info.audio_on_load;
+								document.getElementById("about-the-piece").play();
+								showDiv("piece");
+							}
+						}
+					});
+				}
+				else if (document.getElementById("artist_categories").style.visibility != "hidden")
+				{
+					for (prop in current_piece.artist_info) {
+						alert(prop);
+						if (result.search(prop) > -1)
+						{
+							if (result.match(prop) {
+								document.getElementById("subcategory").src = current_piece.artist_info.name;
+								document.getElementBy
+							}
+							else if (result.match(prop) == "biography"){
+								document.getElementById("subcategory").src = current_piece.artist_info.biography;
+								document.getElementById("subcategory").play();
+							}
+							else if (result.match(prop) == "career") {
+								document.getElementById("subcategory").src = current_piece.artist_info.career;
+								document.getElementById("subcategory").play();
+							}
+						}
+					}
+				}
+				else if (document.getElementById("piece_categories").style.visibility != "hidden")
+				{
+				
+					for (prop in current_piece.piece_info) {
+						if (result.search(prop) > -1)
+						{
+							if (result.match(prop) == "style"){
+								document.getElementById("subcategory").src = current_piece.piece_info.style;
+								document.getElementById("subcategory").play();
+							}
+							else if (result.match(prop) == "medium") {
+								alert(utterance);
+								document.getElementById("subcategory").src = current_piece.piece_info.medium;
+								document.getElementById("subcategory").play();
+							}
+							else if (result.match(prop) == "year") {
+								alert(utterance);
+								document.getElementById("subcategory").src = current_piece.piece_info.year;
+								document.getElementById("subcategory").play();
+							}
+							else if (result.match(prop) == "dimensions") {
+								alert(utterance);
+								document.getElementById("subcategory").src = current_piece.piece_info.dimensions;
+								document.getElementById("subcategory").play();
+							}
+							else if (result.match(prop) == "summary") {
+								alert(utterance);
+								document.getElementById("subcategory").src = current_piece.piece_info.summary;
+								document.getElementById("subcategory").play();
+							}
+							else if (result.match(prop) == "title") {
+								alert(utterance);
+								document.getElementById("subcategory").src = current_piece.piece_info.title;
+								document.getElementById("subcategory").play();
+							}
+						
+							
+						}
+					}
+				}		
+			});					
+		}
+	
 
 }
